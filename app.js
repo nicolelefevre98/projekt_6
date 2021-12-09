1ae1759462aa9bcfb4686f8e55ef9f484e2f7407
Nikolaihesel <Nikolaihesel@icloud.com>
2021-12-09 15:55:11 +0100

    js

--- app.js
+++ app.js
@@ -11,5 +11,29 @@ function showMenu() {
 
 function closeMenu() {
     document.getElementById('mobileMenu').style.display = "none";
+};
+
+
+
+function confirmMessage() {
+    const firstName = document.getElementById('fName').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phoneNumber').value;
+
+    const contactInfo = [firstName, email, phone];
+    
+
+
 }
 
+
+
+
+//stop reload 
+const form = document.getElementById("form-contact");
+
+function handleForm(event) {
+     event.preventDefault(); 
+     console.log('tests')} 
+
+form.addEventListener('submit', handleForm);
