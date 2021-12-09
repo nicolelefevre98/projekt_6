15abba5fd700059de058943079f4b6893fec0507
Nikolaihesel <Nikolaihesel@icloud.com>
2021-12-09 19:58:43 +0100

    Nikolai js virker, fixet nicoles fejl (:

--- app.js
+++ app.js
@@ -15,25 +15,58 @@ function closeMenu() {
 
 
 
-function confirmMessage() {
-    const firstName = document.getElementById('fName').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phoneNumber').value;
-
-    const contactInfo = [firstName, email, phone];
-    
 
 
+
+function confirmMessage(){
+
+  
+
+
+   const firstName = document.getElementById('fName').value;
+   const email = document.getElementById('email').value;
+   const phone = document.getElementById('phoneNumber').value;
+   const messageInput = document.getElementById('msg-input').value;
+
+   const modalConfirm = document.getElementById('closeme');
+  
+
+const contactInfo = [firstName, email, phone, messageInput];
+
+modalConfirm.style.display="flex"
+const menu = document.getElementById('menu');
+contactInfo.forEach((field)=> {
+
+  menu.appendChild(createMenuItem(field));
 }
+)
+};
+
+
+function createMenuItem(field) {
+  let li = document.createElement('li');
+  li.classList.add("confirm-written-msg");
+  li.textContent = field;
+  return li;
+};
+
+function closeModal() {
+  document.getElementById('closeme').style.display = "none";
+};
+
 
 
 
 
 //stop reload 
-// const form = document.getElementById("form-contact");
+const form = document.getElementById("form-contact");
 
-// function handleForm(event) {
-//      event.preventDefault(); 
-//      console.log('tests')} 
+function handleForm(event) {
+     event.preventDefault(); } 
 
-// form.addEventListener('submit', handleForm);
+form.addEventListener('submit', handleForm);
+
+
+
+
+
