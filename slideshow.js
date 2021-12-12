77a93348ffdb48126f8b941df5a87be063291786
stefanjacobsen <stja32075@edu.ucl.dk>
2021-12-12 22:09:22 +0100

    ændret på bredde af slide container

--- slideshow.js
+++ slideshow.js
@@ -14,9 +14,9 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
+  var slides = document.getElementsByClassName("mySlides"); //referer til HTML slides
+  var dots = document.getElementsByClassName("dot"); //referer til HTML prikkerne "dot"
+  if (n > slides.length) {slideIndex = 1} //når funk
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
