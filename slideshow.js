affea4f2777dedf0c54a9f57cfb473218c2c4eba
stefanjacobsen <stja32075@edu.ucl.dk>
2021-12-14 13:59:55 +0100

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -14,16 +14,18 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides"); //referer til HTML slides
-  var dots = document.getElementsByClassName("dot"); //referer til HTML prikkerne "dot"
-  if (n > slides.length) {slideIndex = 1} //når funk
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  var slides = document.getElementsByClassName("mySlides"); //80 referer til HTML slides
+  var dots = document.getElementsByClassName("dot"); // 175 referer til HTML prikkerne "dot"
+  
+  if (n > slides.length) {slideIndex = 1} //når til enden begynd forfra
+  if (n < 1) {slideIndex = slides.length} // når til den anden ende begynd forfra
+
+  for (i = 0; i < slides.length; i++) { //++ er forøgelsen med 1 hver gang
+      slides[i].style.display = "none"; //ccs gemmer alle slide fra DOM
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (i = 0; i < dots.length; i++) { //++ er forøgelsen med 1 hver gang
+      dots[i].className = dots[i].className.replace(" active", ""); //display none i css
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block"; //css
   dots[slideIndex-1].className += " active";
 }
