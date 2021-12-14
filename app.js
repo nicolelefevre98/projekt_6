a76e1c3b8b16bdf34855bc5cf5d05cf782d5c9cc
Nikolaihesel <Nikolaihesel@icloud.com>
2021-12-14 11:32:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //viser mobile menu når burger menu kommer frem
 function showMenu() {
     const mobileMenu = document.getElementById('mobileMenu');
-
+ 
     if (mobileMenu.style.display === "block") {
         mobileMenu.style.display = "none";
       } else {
@@ -25,10 +25,12 @@ function confirmMessage(){
    const phone = document.getElementById('phoneNumber').value;
    const messageInput = document.getElementById('msg-input').value;
 
-   const modalConfirm = document.getElementById('closeme');
   
 //array der samler værdier fra inputfields
 const contactInfo = [firstName, email, phone, messageInput];
+
+//Modal hvor value skal printes
+const modalConfirm = document.getElementById('closeme');
 //ændrer modal fra display none til display flex for at gøre det synligt
 modalConfirm.style.display="flex"
 //opretter menuen hvor punkterne skal laves
@@ -42,20 +44,21 @@ contactInfo.forEach((inputValue) => {
   li.textContent = inputValue;
 
   //henviser til funktionen under der skaber et html element 
-  menu.appendChild(li);
+   menu.appendChild(li);
 }
 )
 };
 
+//stop reload 
+const form = document.getElementById("form-contact");
+
+function handleForm(event) {    event.preventDefault(); } 
+
+form.addEventListener('submit', handleForm);
 
 
 
-// function createMenuItem(inputValue) {
-//   let li = document.createElement('li');
-//   li.classList.add("confirm-written-msg");
-//   li.textContent = inputValue;
-//   return li;
-//};
+
 //"lukker" modal igennem css style change
 function closeModal() {
   document.getElementById('closeme').style.display = "none";
